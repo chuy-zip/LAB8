caa074f5dbc1df1929d6bd87872f7459f8a8443f
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-10-17 09:54:27 -0600

    PcDetails done

--- EjemploFireBaseUVG/app/src/main/java/com/example/ejemplofirebaseuvg/PcDetailsActivity.kt
+++ EjemploFireBaseUVG/app/src/main/java/com/example/ejemplofirebaseuvg/PcDetailsActivity.kt
@@ -2,10 +2,41 @@ package com.example.ejemplofirebaseuvg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class PcDetailsActivity : AppCompatActivity() {
+    private lateinit var tvPcModel: TextView
+    private lateinit var tvPcBrand: TextView
+    private lateinit var tvPcProcessor: TextView
+    private lateinit var tvPcRAM: TextView
+    private lateinit var tvPcROM: TextView
+    private lateinit var btnUpdate: Button
+    private lateinit var btnDelete: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pc_details)
+
+        initView()
+        setValuesToViews()
+    }
+
+    private fun initView() {
+        tvPcModel = findViewById(R.id.tvPcModel)
+        tvPcBrand = findViewById(R.id.tvPcBrand)
+        tvPcProcessor = findViewById(R.id.tvPcProcessor)
+        tvPcRAM = findViewById(R.id.tvPcRAM)
+        tvPcROM = findViewById(R.id.tvPcROM)
+
+        btnUpdate = findViewById(R.id.btnUpdate)
+        btnDelete = findViewById(R.id.btnDelete)
+    }
+
+    private fun setValuesToViews() {
+        tvPcModel.text = intent.getStringExtra("pcModel")
+        tvPcBrand.text = intent.getStringExtra("pcBrand")
+        tvPcProcessor.text = intent.getStringExtra("pcProcessor")
+        tvPcRAM.text = intent.getStringExtra("pcRAM")
+        tvPcROM.text = intent.getStringExtra("pcROM")
     }
 }
